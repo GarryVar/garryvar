4321f6d3fc12270a2664ffdcbafd11a343332152
GarryVar <darisvet.sokolov@yandex.ru>
2021-04-06 18:12:51 +0300

    Added scripts to prjects

---
+++ src/scripts/util.js
@@ -0,0 +1,27 @@
+const SELECTOR_CLASSES = {
+  hidden: 'hidden',
+  navigation: 'header__nav',
+  burgerIcon: 'toggle__icon',
+  headerWrapper: 'header__wrapper'
+};
+
+const nav = document.querySelector(`.${SELECTOR_CLASSES.navigation}`);
+const navBrgButtonIcon = document.querySelector(`.${SELECTOR_CLASSES.burgerIcon}`);
+
+
+const toggleStates = (elem, selector) => {
+  !elem.classList.contains(selector) ? elem.classList.add(selector) : elem.classList.remove(selector)
+};
+toggleStates(nav, SELECTOR_CLASSES.hidden);
+
+
+const onToggleNavigation = evt => {
+  evt.preventDefault();
+  const header = document.querySelector(`.${SELECTOR_CLASSES.headerWrapper}`);
+  toggleStates(nav, SELECTOR_CLASSES.hidden);
+  Array.from(navBrgButtonIcon.children).forEach(item => toggleStates(item, SELECTOR_CLASSES.hidden))
+};
+
+
+const toggleBtn = document.querySelector('.toggle');
+toggleBtn.addEventListener('click', onToggleNavigation);
