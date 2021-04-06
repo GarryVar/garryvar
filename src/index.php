a26525b3b00b98a015d1e885b9b8f875098d2a43
GarryVar <darisvet.sokolov@yandex.ru>
2021-04-06 18:13:28 +0300

    Added index.php to project

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<?php
+	$title = 'garryvar.ru';
+	$alt = 'logo';
+	$nav_links = ['home' => 'index.php', 'sedona' => 'http://garryvar.ru/sedona/', 'sounds' => 'http://garryvar.ru/siod/', 'Graphics'];
+
+	$released_images_urls = [
+	  'dancing_mavka' => 'img/covers/released_dancing_mavka.jpg',
+    'gamayun_tale' => 'img/covers/released_gamayun_tale.jpg',
+    'spiritual_doping' => 'img/covers/released_spiritual_doping.jpg',
+    'goa_meditation_vol3' => 'img/covers/released_goa_meditation_vol3.jpg'
+    ];
+
+	//Links
+	$dancing_mavka_url = 'https://www.discogs.com/Various-Dancing-Mavka/master/350184';
+	$gamayun_tale_url = 'https://www.discogs.com/Various-Gamayun-Tale/master/663801';
+  $spiritual_doping_url = '';
+  $goa_meditation_vol3 = 'https://www.discogs.com/Sky-Technology-Goa-Meditation-vol-3/release/16929132';
+
+
+	require('components/header.php');
+	require('components/main.php');
+	require('components/footer.php');
